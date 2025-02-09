e43240dfbd916cbbe9cfab98e2a7ed7e1a63cd2c
TheHeartJunction <THEHEARTJUNCTION@GMAIL.COM>
2025-02-09 12:32:16 +0530

    Update ThreeDGiftBox.tsx

--- src/components/ThreeDGiftBox.tsx
+++ src/components/ThreeDGiftBox.tsx
@@ -1,1 +1,25 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+const GiftBox = () => {
+  return (
+    <mesh rotation={[0, 0, 0]}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color="pink" />
+    </mesh>
+  );
+};
+
+const ThreeDGiftBox = () => {
+  return (
+    <Canvas camera={{ position: [3, 3, 3] }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[10, 10, 10]} />
+      <GiftBox />
+      <OrbitControls />
+    </Canvas>
+  );
+};
+
+export default ThreeDGiftBox;
 
