f56e9a372192cc7596f040e87a6db43a4605df79
TheHeartJunction <THEHEARTJUNCTION@GMAIL.COM>
2025-02-11 22:31:11 +0530

    Update FeaturedGiftsCarousel.tsx

--- src/components/FeaturedGiftsCarousel.tsx
+++ src/components/FeaturedGiftsCarousel.tsx
@@ -4,7 +4,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { product } from "@/src/pages/Products.tsx";
+import { products } from "@/src/pages/Products.tsx";
 interface Product {
   id: number;
   name: string;
